f2102e3e9c2c4b0071be41823f7fa390ffe3a09a
Himanshu <himanshu.jmy@gmail.com>
2021-04-28 22:22:51 +0530

    Create Reverse_String.py
    Reverse a sequence of string

---
+++ Reverse_String.py
@@ -0,0 +1,9 @@
+# Reverse a sequence of string
+y = input("Enter string which needs to be reverted: ")
+
+def string(x):
+    return x[::-1]
+
+reversed_string = string(y)
+
+print("Reversed string is: ",reversed_string )
