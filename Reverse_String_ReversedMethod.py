e954af423a274934cafad18fb1f4a2b86293ade2
Himanshu <himanshu.jmy@gmail.com>
2021-04-29 19:42:45 +0530

    Create Reverse_String_ReversedMethod.py
    Reverse a sequence of string using reversed method

---
+++ Reverse_String_ReversedMethod.py
@@ -0,0 +1,8 @@
+# Reverse a sequence of string using reversed method
+string = input("Enter string: ")
+
+print("\n")
+print("Original string is: ", string)
+
+reversed_string = ''.join(reversed(string))
+print("Reversed of given string is: ",reversed_string)
