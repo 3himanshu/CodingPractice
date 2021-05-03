11bfbedf8e02f40350265d6cfd47755ca9637e3c
Himanshu <himanshu.jmy@gmail.com>
2021-05-03 18:11:23 +0530

    Create SquareRoot.py
    # Python Program to Find the Square Root

---
+++ SquareRoot.py
@@ -0,0 +1,5 @@
+# Python Program to Find the Square Root
+import numpy as np
+number = int(input("Enter number: "))
+sqrt = np.sqrt(number)
+print("The square root of {0:0.3f} is {1:0.3f}".format(number ,sqrt))
