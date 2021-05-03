d77a07a0423afc052d3fa7dfae8741d9c6a6d372
Himanshu <himanshu.jmy@gmail.com>
2021-05-03 18:16:44 +0530

    Create SquareRoot_Exponent.py
    # Python Program to Find the Square Root using the ** exponent operator

---
+++ SquareRoot_Exponent.py
@@ -0,0 +1,7 @@
+# Python Program to Find the Square Root
+
+number = float(input("Enter number: "))
+sqrt = number ** 0.5
+
+# print('The square root of %0.3f is %0.3f'%(number ,sqrt))
+print("The square root of {0:0.3f} is {1:0.3f}".format(number ,sqrt))
