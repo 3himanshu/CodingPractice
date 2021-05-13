e2866ed74dbc908737bf98d7c848fff58d3cd9e9
Himanshu <himanshu.jmy@gmail.com>
2021-05-13 19:52:00 +0530

    Create SwapTwoVariablesWithoutUsingTemporaryVariable_2.py
    Python Program to Swap Two Variables using arithmetic operator only if provided variables are number

---
+++ SwapTwoVariablesWithoutUsingTemporaryVariable_2.py
@@ -0,0 +1,12 @@
+# Python Program to Swap Two Variables using arithmetic operator only if provided variables are number
+
+var1 = int(input("Enter first number: "))
+
+var2 = int(input("Enter second number: "))
+
+var1 = var1 + var2
+var2 = var1 - var2
+var1 = var1 - var2
+
+print("after swapping, var1 is: ", var1)
+print("after swapping, var2 is: ", var2)
