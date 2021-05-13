653aaa1c628cf754ee20c8a3ec46b6cd1199b924
Himanshu <himanshu.jmy@gmail.com>
2021-05-13 19:51:17 +0530

    Create SwapTwoVariablesWithoutUsingTemporaryVariable_1.py
    Python Program to Swap Two Variables without using a temporary variable

---
+++ SwapTwoVariablesWithoutUsingTemporaryVariable_1.py
@@ -0,0 +1,10 @@
+# Python Program to Swap Two Variables without using a temporary variable 
+
+var1 = input("Enter first number: ")
+
+var2 = input("Enter second number: ")
+
+var1 , var2 = var2 , var1
+
+print("after swapping, var1 is: ", var1)
+print("after swapping, var2 is: ", var2)
