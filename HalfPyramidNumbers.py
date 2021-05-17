26ea3380f23325328f5b144d34aadca0c658e111
Himanshu <himanshu.jmy@gmail.com>
2021-05-17 13:45:33 +0530

    Create HalfPyramidNumbers.py
    Program to print half pyramid using numbers

---
+++ HalfPyramidNumbers.py
@@ -0,0 +1,8 @@
+# Program to print half pyramid using numbers
+
+rows = int(input("Enter number of rows for half pyramid: "))
+
+for i in range(rows):
+    for j in range(i + 1):
+        print(j+1," ", end="")
+    print("\n")
